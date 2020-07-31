b0bcace60b47d089939952b872b8cb77d17d868c
Kwok <kwokrenx@gmail.com>
2020-07-30 23:54:34 -0400

    successfully seeded my food.js file

--- fitnessAPI/seed/food.js
+++ fitnessAPI/seed/food.js
@@ -1,2 +1,26 @@
 const db = require('../db/connection.js');
 const Food = require('../models/food.js');
+const mongoose = require('../db/connection.js');
+
+const foodSeed = [
+    {
+        breakfast: ["Boiled Eggs", "Fruits", "Oatmeal"],
+        lunch: ["Chicken Breast", "White Rice", "Caesar Salad"],
+        dinner: ["Spinach", "Steak", "Potatoes"]
+    },
+    {
+        breakfast: ["Scrambled Eggs", "Chia Pudding", "Peanut Butter Sandwich"],
+        lunch: ["Chicken Thighs", "Spaghetti"],
+        dinner: ["Mash Potatoes", "Peas", "Corn"]
+    }
+]
+
+Food.insertMany(foodSeed,
+    (err, res) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(res);
+        }
+        mongoose.connection.close()
+    })
