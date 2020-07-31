0057a6454ea2ccfe6c6fd5ff089201fbcae8d1fd
Kwok <kwokrenx@gmail.com>
2020-07-31 14:26:23 -0400

    added a create route to create a new entry

--- controllers/fitness.js
+++ controllers/fitness.js
@@ -11,4 +11,14 @@ const index = async (req, res) => {
     }
 }
 
-module.exports = {index}
+const create = async (req, res) => {
+    try {
+        const createFitness =  await Fitness.create(req.body);
+        res.status(200).json(createFitness);
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+module.exports = {index, create}
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fitnessRouter = express.Router();
-const {index} = require('../controllers/fitness');
+const {index, create} = require('../controllers/fitness');
 const fitness = require('../controllers/fitness');
 
 fitnessRouter.get('/', index);
+
+fitnessRouter.post('/', create);
 
 module.exports = fitnessRouter
