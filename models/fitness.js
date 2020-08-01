d5d50d1e4acb3653d220a10a2e60f9a4a56c06e4
Kwok <kwokrenx@gmail.com>
2020-08-01 17:17:30 -0400

    changed back since it didn't work

--- models/fitness.js
+++ models/fitness.js
@@ -11,7 +11,7 @@ const fitnessSchema = new Schema(
             reps: Number,
             directionVideo: String,
         }],
-        food: [{type:Schema.Types.ObjectId, ref:'foods'}]
+        food: [{type:Schema.Types.ObjectId, ref:'food'}]
     }
 );
 
